7321879d4aca04fd82f25010f112084c3d84252c
Tuna <mac@tunasmac.local>
2023-04-30 15:54:56 +0300

    selecting piecharts implemented

--- frontend/src/components/portfolio_components/piechart.jsx
+++ frontend/src/components/portfolio_components/piechart.jsx
@@ -4,25 +4,22 @@ import "./piechart.css";
 
 class Piechart extends Component {
   state = {
-    isHighlighted: false,
-    name: "Deneme",
-    data: [
-      { name: "North", value: 100 },
-      { name: "South", value: 200 },
-      { name: "East", value: 300 },
-      { name: "West", value: 400 },
-      { name: "T", value: 500 },
-      { name: "T2", value: 500 },
-      { name: "T3", value: 500 },
-      { name: "T4", value: 500 },
-    ],
+    chartID:"",
+    isSelected: "",
+    name: "",
+    data: [],
   };
 
+  
+
+
   handleClick = () => {
-    this.setState((prevState) => ({
-      isHighlighted: !prevState.isHighlighted,
-    }));
+    alert(this.props.isSelected);
+    // this.props.isSelected = true;
+    this.props.onClick(this.props.chartID);
+    
   };
+
 
   generateColors = (length) => {
     const COLORS = [
@@ -85,20 +82,25 @@ class Piechart extends Component {
   };
 
   render() {
-    const colors = this.generateColors(this.state.data.length);
-    const containerClass = this.state.isHighlighted ? "HighlightedContainer" : "PiechartContainer";
+    const colors = this.generateColors(this.props.data.length);
+    const containerClass = this.props.isSelected
+      ? "HighlightedContainer"
+      : "PiechartContainer";
 
     return (
       <div
         className={`PiechartContainer ${containerClass}`}
         style={{ width: "400px" }}
       >
-        <h3 className="PiechartName" onClick={this.handleClick}>
-          {this.state.name}
+        <h3 className="PiechartName" onClick={() => {
+          this.props.onClick(this.props.chartID);
+          
+        }}>
+          {this.props.name}
         </h3>
         <PieChart width={250} height={250}>
           <Pie
-            data={this.state.data}
+            data={this.props.data}
             dataKey="value"
             nameKey="name"
             cx="50%"
@@ -107,7 +109,7 @@ class Piechart extends Component {
             fill="#8884d8"
             labelLine={false}
           >
-            {this.state.data.map((entry, index) => (
+            {this.props.data.map((entry, index) => (
               <Cell key={`cell-${index}`} fill={colors[index]} />
             ))}
           </Pie>
--- frontend/src/components/portfolio_components/piechartHolder.jsx
+++ frontend/src/components/portfolio_components/piechartHolder.jsx
@@ -5,35 +5,93 @@ import Piechart from "./piechart";
 class PiechartHolder extends Component {
   state = {
     piecharts: [],
+    selectedChart: null,
     count: 0,
   };
 
+  handlePiechartClick = (chartId) => {
+    const piecharts = this.state.piecharts;
+    const newPiecharts = piecharts.map(piechart => {
+      if (piechart.props.chartID === chartId) {
+        return React.cloneElement(piechart, { isSelected: true });
+      } else {
+        return React.cloneElement(piechart, { isSelected: false });
+      }
+    });
+    // this.state.piecharts = newPiecharts;
+    this.state.selectedChart = chartId;
+    this.setState({ piecharts: newPiecharts});
+    console.log(this.state.selectedChart);
+  };
+  
   
 
   render() {
     return (
-      <div style={{display: "flex",}}>
-        <Piechart></Piechart>
-        <Piechart></Piechart>
-        <Piechart></Piechart>
-        <Piechart></Piechart>
-        <Piechart></Piechart>
-        <Piechart></Piechart>
-        <Piechart></Piechart>
-
-        
+      <div style={{ display: "flex" }}>
+        <div style={{ display: "flex" }} className="">
+          {this.state.piecharts}
+        </div>
       </div>
     );
   }
   componentWillMount() {
-    axios.get("http://localhost:8000/api/get_portfolios").then((res) => {
-        params: { id: localStorage.getItem("userToken") }
+    // axios.get("http://localhost:8000/api/get_portfolios").then((res) => {
+    //     params: { id: localStorage.getItem("userToken") }
 
-    });
+    // });
+    let finalList = [];
+    const data = [
+      { name: "North", value: 100 },
+      { name: "South", value: 200 },
+      { name: "East", value: 300 },
+    ];
+    const data2 = [
+      { name: "North", value: 100 },
+      { name: "South", value: 200 },
+      { name: "East", value: 900 },
+    ];
+    const data3 = [
+      { name: "North", value: 100 },
+      { name: "South", value: 200 },
+      
+    ];
 
-    
+    finalList.push(
+      <Piechart
+        chartID={1}
+        // isSelected={1 === this.selectedChart}
+        name={"Deneme1"}
+        data={data}
+        onClick={() => this.handlePiechartClick(1)}
+      />
+    );
+    finalList.push(
+      <Piechart
+        chartID={2}
+        // isSelected={2 === this.selectedChart}
+        name={"Deneme2"}
+        data={data2}
+        onClick={() => this.handlePiechartClick(2)}
+      />
+    );
+    finalList.push(
+      <Piechart
+        chartID={3}
+        // isSelected={2 === this.selectedChart}
+        name={"Deneme3"}
+        data={data2}
+        onClick={() => this.handlePiechartClick(3)}
+      />
+    );
+
+    // for (var i = 0; i < finalList.length; i++) {
+    //   if (finalList[i].isHighlited === true) {
+    //     this.state.highlitedKey = finalList[i].key;
+    //   }
+    // }
+    this.setState({ piecharts: finalList });
   }
-  
 }
 
 export default PiechartHolder;
