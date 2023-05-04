4e149fd9f1e884695f2fd30da48cef13624ec869
Yesim Hizir <yesimhizir@Yesims-MacBook-Pro.local>
2023-05-04 21:12:55 +0300

    retrieving data for my-portfolio page

--- myapp/dbFunctions.py
+++ myapp/dbFunctions.py
@@ -28,5 +28,6 @@ def get_asset_information_with_ids(user_asset_ids):
     return(Assets)
 
 def get_portfolios_with_user_id(id):
-    portfolios = my_custom_sql("SELECT asset_information.name,asset_information.current_value,user_asset_ownership.portfolio_id,user_asset_ownership.asset_id,user_asset_ownership.amount FROM comp491.user_asset_ownership,comp491.asset_information WHERE user_asset_ownership.asset_id = asset_information.asset_id and user_asset_ownership.user_id={};".format(id),connection)
+    # portfolios = my_custom_sql("SELECT asset_information.name,asset_information.current_value,user_asset_ownership.portfolio_id,user_asset_ownership.asset_id,user_asset_ownership.amount FROM comp491.user_asset_ownership,comp491.asset_information WHERE user_asset_ownership.asset_id = asset_information.asset_id and user_asset_ownership.user_id={};".format(id),connection)
+    portfolios = my_custom_sql("SELECT asset_information.name, asset_information.current_value, user_asset_ownership.portfolio_id, user_asset_ownership.asset_id, user_asset_ownership.amount, portfolio.name FROM comp491.user_asset_ownership, comp491.asset_information,  comp491.portfolio WHERE user_asset_ownership.asset_id = asset_information.asset_id and user_asset_ownership.user_id={};".format(id),connection)
     return(portfolios)
