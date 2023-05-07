8999459c4e81353178e3f3cd24ac30f96d5fb2ab
Tuna <mac@tunasmac.local>
2023-05-07 20:14:59 +0300

    add new symbol to portfolio axios implemented needs debug with backned

--- frontend/src/components/portfolio_components/modifyPortfolio.jsx
+++ frontend/src/components/portfolio_components/modifyPortfolio.jsx
@@ -1,26 +1,57 @@
 import React, { Component } from "react";
 import "./modifyPortfolio.css";
+import axios from "axios";
 
 class ModifyPortfolio extends Component {
   state = {
-    name: "",
-    currency: "",
+    searchTerm: "",
+    amount1: "",
+    amount2: "",
+    searchResults: [],
+    selectedSymbol: "",
   };
 
   handleNameChange = (event) => {
     this.setState({ name: event.target.value });
   };
 
-  handleCurrencyChange = (event) => {
-    this.setState({ currency: event.target.value });
+  handleAmount1Change = (event) => {
+    this.setState({ amount1: event.target.value });
   };
 
   handleSubmit = (event) => {
     event.preventDefault();
+    axios
+      .post("http://localhost:8000/api/add-to-portfolio", {
+        symbol: this.state.selectedSymbol,
+        amount: this.state.amount1,
+      })
+      .then(function (response) {
+        if (response.status === 201) {
+          alert(localStorage.getItem("selectedSymbol") + "added to portfolio");
+        }
+      })
+      .catch(function (error) {
+        alert("Could not create potfolio please check your inputs");
+      })
+      .finally((response) => {});
+
     // call function to handle form submission here
   };
 
   render() {
+    const possibleItems = [
+      { name: "TSLA" },
+      { name: "MSLA" },
+      { name: "TSLB" },
+      { name: "TSGA" },
+    ];
+    let filteredItems = possibleItems;
+    if (this.state.searchTerm) {
+      filteredItems = possibleItems.filter((item) =>
+        item.name.toLowerCase().startsWith(this.state.searchTerm.toLowerCase())
+      );
+    }
     return (
       <div className="ModifyPortfolioContainer">
         <h1 className="PortfolioNameModifyPortfolio">
@@ -35,7 +66,28 @@ class ModifyPortfolio extends Component {
                 type="search"
                 placeholder="Search for symbols"
                 aria-label="Search"
+                onChange={(event) =>
+                  this.setState({ searchTerm: event.target.value })
+                }
               ></input>
+              {this.state.searchTerm &&
+                filteredItems.map((item) => (
+                  <button
+                    style={{
+                      borderRadius: "10px",
+                      color: "white",
+                      backgroundColor: "#7498da",
+                    }}
+                    onClick={(event) => {
+                      event.preventDefault();
+                      this.setState({ selectedSymbol: item.name });
+                      // localStorage.setItem("selectedSymbol",item.name);
+                      this.setState({ searchTerm: item.name });
+                    }}
+                  >
+                    {item.name}
+                  </button>
+                ))}
             </form>
             <div className="AmountModifyPortfolio">
               {/* <h>Amount: </h> */}
@@ -43,9 +95,12 @@ class ModifyPortfolio extends Component {
                 className="AmountField"
                 type="text"
                 placeholder="Amount:"
+                onChange={this.handleAmount1Change}
               />
             </div>
-            <button className="AddSymbolButton">Add</button>
+            <button onClick={this.handleSubmit} className="AddSymbolButton">
+              Add
+            </button>
           </div>
           <div className="separator"></div>
           <div className="ModifyPortfolioRight">
@@ -62,13 +117,23 @@ class ModifyPortfolio extends Component {
               />
             </div>
             <button className="ChangeSymbolButton">+</button>
-            <h1 style={{fontSize: "2vh"}}>or</h1>
+            <h1 style={{ fontSize: "2vh" }}>or</h1>
             <button className="RemoveSymbol">-</button>
-
           </div>
         </div>
       </div>
     );
+  }
+  componentWillMount() {
+    axios
+      .get("http://localhost:8000/api/all-symbols", {})
+      .then((response) => {
+        localStorage.setItem("allSymbols", response.data);
+        this.searchResults = response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 }
 
